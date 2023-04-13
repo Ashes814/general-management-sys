8d2a665ca45ea032deb1f84be109972f7f495774
张欧文 <1534661514@qq.com>
2023-04-13 18:36:50 +0800

    route template

--- GM-system/src/App.tsx
+++ GM-system/src/App.tsx
@@ -1,16 +1,15 @@
 import { useState } from "react";
-
-import { Button } from "antd";
-import { VerticalRightOutlined } from "@ant-design/icons";
+import { useRoutes, Link } from "react-router-dom";
+import router from "./router";
 
 function App() {
   const [count, setCount] = useState(0);
+  const outlet = useRoutes(router);
 
   return (
     <div className="App">
-      <Button type="primary">button</Button>
-
-      <VerticalRightOutlined style={{ fontSize: "40px", color: "red" }} />
+      <Link to="/home">Home</Link> | <Link to="/about">About</Link>
+      {outlet}
     </div>
   );
 }
